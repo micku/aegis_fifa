be5e5d064c4650691c70008c8436b675b44ed961
micku <michele@michelegargiulo.info>
2013-12-04 17:21:41 +0100

    test wsgi 3

--- aegis_fifa/wsgi.py
+++ aegis_fifa/wsgi.py
@@ -36,7 +36,7 @@ application = Cling(get_wsgi_application())
 # application = HelloWorldApplication(application)
 """
 import os
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 #from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aegis_fifa.settings")
